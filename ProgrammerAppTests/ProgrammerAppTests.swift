30c069ee88bbef9fbd8e9fe566d82fe5acb0667f
Requime31 <rs6063187@gmail.com>
2026-02-07 14:07:38 +0100

    Polishing the app

--- ProgrammerAppTests/ProgrammerAppTests.swift
+++ ProgrammerAppTests/ProgrammerAppTests.swift
@@ -10,27 +10,64 @@ import XCTest
 
 final class ProgrammerAppTests: XCTestCase {
 
+    private var persistence: PersistenceService!
+    private let suiteName = "ProgrammerAppTests"
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        let defaults = UserDefaults(suiteName: suiteName)!
+        defaults.removePersistentDomain(forName: suiteName)
+        persistence = PersistenceService(userDefaults: defaults)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        persistence = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // MARK: - PersistenceService primitive types
+
+    func testPersistenceService_IntRoundTrip() throws {
+        persistence.set(42, forKey: "testInt")
+        XCTAssertEqual(persistence.getInt(forKey: "testInt"), 42)
+        persistence.remove(forKey: "testInt")
+        XCTAssertEqual(persistence.getInt(forKey: "testInt"), 0)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testPersistenceService_BoolRoundTrip() throws {
+        persistence.set(true, forKey: "testBool")
+        XCTAssertTrue(persistence.getBool(forKey: "testBool"))
+        persistence.set(false, forKey: "testBool")
+        XCTAssertFalse(persistence.getBool(forKey: "testBool"))
+    }
+
+    func testPersistenceService_StringRoundTrip() throws {
+        persistence.set("hello", forKey: "testString")
+        XCTAssertEqual(persistence.getString(forKey: "testString"), "hello")
+        persistence.remove(forKey: "testString")
+        XCTAssertNil(persistence.getString(forKey: "testString"))
+    }
+
+    func testPersistenceService_DoubleRoundTrip() throws {
+        persistence.set(3.14, forKey: "testDouble")
+        XCTAssertEqual(persistence.getDouble(forKey: "testDouble"), 3.14, accuracy: 0.001)
+    }
+
+    func testPersistenceService_CodableRoundTrip() throws {
+        struct TestItem: Codable, Equatable {
+            let id: Int
+            let name: String
         }
+        let item = TestItem(id: 1, name: "Test")
+        try persistence.set(item, forKey: "testCodable")
+        let loaded: TestItem? = try persistence.get(TestItem.self, forKey: "testCodable")
+        XCTAssertEqual(loaded, item)
     }
 
+    func testPersistenceService_RemoveClearsValue() throws {
+        persistence.set(100, forKey: "testRemove")
+        XCTAssertEqual(persistence.getInt(forKey: "testRemove"), 100)
+        persistence.remove(forKey: "testRemove")
+        XCTAssertEqual(persistence.getInt(forKey: "testRemove"), 0)
+    }
 }
